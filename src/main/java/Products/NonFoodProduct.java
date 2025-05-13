32a257b6f7087328992e9b95403c5377648bc205
Viktor Rangelov <viktorrangelov0@gmail.com>
2025-05-13 22:29:04 +0300

    adding classes for products and store

--- src/main/java/Products/NonFoodProduct.java
+++
@@ -1,9 +0,0 @@
-package Products;
-
-import java.math.BigDecimal;
-
-public class NonFoodProduct extends Product{
-    public NonFoodProduct(String name, BigDecimal deliveryPrice, Category category){
-        super(name, deliveryPrice, category);
-    }
-}
